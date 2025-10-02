637b5076a73eb79be542462bb2cdb2e481c58cca
Jeremie Gourovitch <jgourovitch@questel.com>
2025-10-02 10:21:11 +0200

    * Updated requirements

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ def callback():
     return f"✅ Logged in with code: {code}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
